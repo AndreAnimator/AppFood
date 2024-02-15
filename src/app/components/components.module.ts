f69fce527598fcb3850f6453da9a055e952c143b
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2024-02-15 11:20:14 -0300

    search and account pages

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { LoadingRestaurantsComponent } from './loading-restaurants/loading-restaurants.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantComponent, LoadingRestaurantsComponent],
+  declarations: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [RestaurantComponent, LoadingRestaurantsComponent]
+  exports: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent]
 })
 export class ComponentsModule { }
