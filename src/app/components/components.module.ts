117fab24488a0c57e0ddfaae6fe73f93b3f17ea5
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2024-02-26 14:43:54 -0300

    Account page componentizada.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,15 +4,18 @@ import { IonicModule } from '@ionic/angular';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { LoadingRestaurantsComponent } from './loading-restaurants/loading-restaurants.component';
 import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
+import { OrderComponent } from './order/order.component';
+import { LoadingOrderComponent } from './loading-order/loading-order.component';
+import { EmptyOrderComponent } from './empty-order/empty-order.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent],
+  declarations: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent, OrderComponent, LoadingOrderComponent, EmptyOrderComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent]
+  exports: [RestaurantComponent, LoadingRestaurantsComponent, EmptyScreenComponent, OrderComponent, LoadingOrderComponent, EmptyOrderComponent]
 })
 export class ComponentsModule { }
